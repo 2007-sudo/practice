c51e4bc3837874c38dc556f916ac486487e62c3f
2007-sudo <71652780+2007-sudo@users.noreply.github.com>
2021-05-31 18:52:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+ var car="priyanka"
+
+ console.log(car.length);
+ console.log(car.toUpperCase())
+
+ if(3>2){
+console.log("ok")
+
+ }
+ else{
+console.log("not ok")
+ }
+
+ var name="my name";
+ switch(name){
+  case "my name": console.log("correct");
+  break;
+  case "My name": console.log("wrong");
+  break;
+ }
+ 
+ for(var i=0; i<=5;i++){
+console.log(i)
+ }
+ var a=["abc","rfgh"];
+ console.log(a[0]);
+a.push("opq");
+console.log(a)
+ console.log(a.length);
+
+ function shape(r){
+   var c= 2*3.14*r;
+   console.log(c)
+ }
+
+ shape(5);
